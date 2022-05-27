d5142789a4724a7a9fa3b3ae7b4c64ae9f025791
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2022-05-27 13:43:17 +0200

    Create model from scratch

--- src/simo/write_simo_task.py
+++ src/simo/write_simo_task.py
@@ -4,13 +4,14 @@ Create a SIMO task from scratch and export as json
 import os
 from pathlib import Path
 from sima.simo.simotask import SIMOTask
+from sima.simo.simomodel import SIMOModel
 from sima.simo.simobody import SIMOBody
 from sima.simo.bodytype import BodyType
 from sima.hydro.structuralmass import StructuralMass
 
 from simapy.sima_writer import SIMAWriter
 
-task = SIMOTask(name="test")
+task = SIMOTask(name="test",model=SIMOModel())
 body = SIMOBody(name="body",_type=BodyType.THREE_DOF_TIME_DOMAIN)
 body.structuralMass = StructuralMass(mass=100.0)
 

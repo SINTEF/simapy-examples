9a78950793a8ad9de674f43110e50a39068396db
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2024-03-11 18:39:02 +0100

    Prototype logging

--- src/workflow/run_from_stask_with_file.py
+++ src/workflow/run_from_stask_with_file.py
@@ -1,6 +1,7 @@
 """
 Imports stask then run the given workflow with input from file
 """
+import re
 import os
 import shutil
 from pathlib import Path
@@ -25,7 +26,7 @@ b.value = [3.0 ,4.0, 5.0]
 SIMAWriter().write([a,b], ws / "input.json")
 
 # Locate stask file in folder "input" at the root of this repository
-stask = Path(__file__).parent / ".." / ".." / "input" / "workflow" / "simple_workflow_run.stask"
+stask = Path(__file__).parent / ".." / ".." / "output" / "wfset.stask"
 
 commands = []
 commands.append("--import")
@@ -34,9 +35,28 @@ commands.append("--run")
 commands.append("task=WorkflowTask")
 commands.append("workflow=outer")
 
+
+def __handle_output(line: str):
+    # A status line looks like this: @STATUS "Total" 100 1000
+    # The first number is the current value, the second is the total
+    pattern = r'@STATUS "(?P<label>\w+)" (?P<current>\d+) (?P<total>\d+)'
+    # Match the pattern in the string
+    match = re.match(pattern, line)
+    if match:
+        label = match.group('label')
+        current = float(match.group('current'))
+        total = float(match.group('total'))
+        progress = int(100*current / total)
+        print(f"Progress: {label} {progress}")
+    else:
+        pass
+        # print(line)
+
+
 # Requires that the environment is set, but an alternative path may be given
 exe =  os.getenv('SRE_EXE')
 sima = SIMA(exe=exe)
+sima.console_handler = __handle_output
 sima.run(ws,commands)
 output = ws / "Output/output.json"
 
@@ -49,8 +69,3 @@ for run_container in run_containers:
     print(f"Run {run_container.name}")
     result: EquallySpacedSignal = run_container.signals[0]
     print(f"{result.name}={result.value}")
-
-
-
-
-
